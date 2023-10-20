4fb532ba9986a8d8451a5f4cec26527342eb4ed1
barroit <sunjiamu@outlook.com>
2023-10-20 23:07:29 +0900

    init user service

---
+++ user-service/src/test/java/com/gbc/service/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbc.service.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
